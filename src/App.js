59799f22e840022418eba58ea024b3b92d78ee27
zubairhmd <zubairhmd84@gmail.com>
2021-11-18 09:51:53 +0600

    done countries component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Countries from './Countries/Countries.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to REST Countries</h1>
+      <Countries></Countries>
     </div>
   );
 }
+
 
 export default App;
